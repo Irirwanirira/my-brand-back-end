f58f8dbb1f9a75d2411a8a75b616d176f2c298f2
Irirwanirira <irijoseph41@gmail.com>
2024-02-13 13:41:55 +0200

    Add message model

---
+++ src/models/messageModel.ts
@@ -0,0 +1,32 @@
+import { Schema, model, Document } from "mongoose";
+
+interface Message extends Document{
+    name: string;
+    email: string;
+    message: string;
+    date: string
+}
+
+const MessageSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now,
+    },
+    time: String
+})
+
+const Messages= model<Message>("Message", MessageSchema)
+
+export default Messages
