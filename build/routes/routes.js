f5ac52ff6f7902840c0cfab4ed4c3b3d0ea82848
Irirwanirira <irijoseph41@gmail.com>
2024-02-14 11:18:02 +0200

    Add Routes for articles

--- build/routes/routes.js
+++ build/routes/routes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 const router = express.Router();
 import { createMessage, getMessages, getUniqueMessage, deleteMessage } from "../controllers/messageController.js";
+import { createArticle, getArticles, getUniqueArticle, updateArticle } from "../controllers/articleController.js";
 router
     .post("/message", createMessage)
     .get("/message", getMessages)
     .get("/message/:id", getUniqueMessage)
-    .delete("/message/:id", deleteMessage);
+    .delete("/message/:id", deleteMessage)
+    .post("/article", createArticle)
+    .get("/article", getArticles)
+    .get("/article/:id", getUniqueArticle)
+    .patch("/article/:id", updateArticle);
 export default router;
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,8 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express from 'express';
 const router = express.Router();
 import {createMessage, getMessages, getUniqueMessage, deleteMessage} from "../controllers/messageController.js"
+import {createArticle, getArticles, getUniqueArticle, updateArticle} from "../controllers/articleController.js";
+
 
 router
     .post("/message", createMessage)
@@ -8,9 +10,10 @@ router
     .get("/message/:id", getUniqueMessage)
     .delete("/message/:id", deleteMessage)
 
-    
-
-
+    .post("/article", createArticle)
+    .get("/article", getArticles)
+    .get("/article/:id", getUniqueArticle)
+    .patch("/article/:id", updateArticle)
 
 export default router
 
