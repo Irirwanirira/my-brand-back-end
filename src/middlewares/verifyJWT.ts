03cc3993f6dee648b51ac9249712be3abc6d7b53
Irirwanirira <irijoseph41@gmail.com>
2024-02-14 21:55:06 +0200

    Create jwt token  for user authorization

---
+++ src/middlewares/verifyJWT.ts
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import pkg from "http-status";
+
+const {UNAUTHORIZED, FORBIDDEN} = pkg;
+
+const verifyJWT = async (req: Request, res: Response, next: NextFunction) => {
+
+    if(req.headers.authorization === undefined || req.headers.authorization === null){
+        return res.status(UNAUTHORIZED).json({ message: 'Unauthorized' });
+    }
+
+    const token = req.headers.authorization.split(' ')[1];
+
+    if(token === undefined || token === null){
+        return res.status(UNAUTHORIZED).json({ message: 'Unauthorized' });
+    }
+    else {
+        jwt.verify(token.substring(1, token.length - 1), 
+        process.env.ACCESS_TOKEN_SECRET!, 
+        (err: any, decoded: any) => {
+            if (err) {
+                return res.status(FORBIDDEN).json({ message: 'Unable to verify the JWT token' + err });
+            }
+            else if(decoded){
+                req.body.email = decoded.userEmail;
+                req.body.role = decoded.roles;
+                next();
+            }else{
+                res.status(UNAUTHORIZED).json({message:"Token might be either expired or invalid, please login again to get a new token"})
+            }
+        });
+    }
+}
+
+export default verifyJWT;
