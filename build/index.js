6f7676e0011ecc2be0faaa7fcf46da91e5291bbb
Irirwanirira <irijoseph41@gmail.com>
2024-02-15 11:01:54 +0200

    Add home api

--- build/index.js
+++ build/index.js
@@ -11,6 +11,9 @@ mongoose
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use("/api", Routes);
+    app.get("/", (req, res) => {
+        res.status(200).json({ Message: "Welcome to my brand new API, I hope you enjoy it." });
+    });
     app.listen(process.env.PORT, () => {
         console.log("Wakanda forever on Atlas port " + PORT);
     });
--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,12 @@ mongoose
 .then(() => {
     const app: Application  = express()
     app.use(express.json())
-    app.use(express.urlencoded({extended: true}))   
+    app.use(express.urlencoded({extended: true}))
+   
     app.use("/api", Routes)
+    app.get("/", (req, res) => {
+        res.status(200).json({Message: "Welcome to my brand new API, I hope you enjoy it."})
+    }) 
     app.listen(process.env.PORT, ()=> {
         console.log("Wakanda forever on Atlas port " + PORT)
     })
